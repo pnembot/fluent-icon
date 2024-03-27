344a09a9717ed4ebb43c37d12cbffd69870512e9
pnembot <143297423+pnembot@users.noreply.github.com>
2024-03-27 05:31:12 +0000

    resolving react errors

--- src/More.tsx
+++ src/More.tsx
@@ -12,7 +12,7 @@ export function More(props: SVGProps<SVGSVGElement>) {
       <path
         d="M4 7a1 1 0 1 1 0 2a1 1 0 0 1 0-2zm4 0a1 1 0 1 1 0 2a1 1 0 0 1 0-2zm4 0a1 1 0 1 1 0 2a1 1 0 0 1 0-2z"
         fill="currentColor"
-        fill-rule="nonzero"
+        fillRule="nonzero"
       />
     </svg>
   );
